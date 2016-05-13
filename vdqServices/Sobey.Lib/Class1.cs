a31b1d39dc2655f7f9bfacc8489ef3c3dd809fa1
xiaohutuxian5212@163.com <xiaohutuxian5212@163.com>
2016-05-13 14:46:59 +0800

    每个模块独立

--- vdqServices/Sobey.Lib/Class1.cs
+++ vdqServices/Sobey.Lib/Class1.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace vdqServices.asdfsd
+namespace Sobey.Lib
 {
-    class test
+    public class Class1
     {
     }
 }
